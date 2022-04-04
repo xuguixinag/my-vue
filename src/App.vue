1b5af8b61c67a8098ccf28466cbcfd9d36575a44
liubin <liub@robooot.com>
2022-04-04 19:05:04 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+    shemewanyi 
+    <First/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import First from './components/First.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    First
+  }
+}
+</script>
+
+<style>
+
+</style>
